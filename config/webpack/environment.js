181a6423d9902b859675e622eb6fd63654847753
mmr-p <morimoto3377@gmail.com>
2021-01-18 22:35:15 +0900

    import bootstrap

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
+// jQueryとBootstap用
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: 'popper.js'
+  })
+)
+
 module.exports = environment
